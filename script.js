8f7d3b9288dba5ab6b288cfbd1bd0573b8755edc
Saluke90 <saluke90@gmail.com>
2024-03-17 16:24:15 +0100

    grid done and button

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-function pina() {
-    console.log("pina");
-}
+const container = document.querySelector(".container");
 
-pina();
+function gridMaker(num) {
+    for (let i = 0; i < num; i++) {
+        let col = document.createElement("div");
+        container.appendChild(col);
+        col.classList.add("col");
+    };
+
+    const columns = document.querySelectorAll(".col");
+
+    for (let j = 0; j < num; j++) {
+        columns.forEach((column) => {
+            let row = document.createElement("div");
+            column.appendChild(row);
+            row.classList.add('row');
+        }
+    )}
+};
+
+gridMaker(64);
+
+const squares = document.querySelectorAll(".row");
+
+squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "red";
+    })
+})
